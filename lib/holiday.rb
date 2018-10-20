b389069cfbacbbad26559680b11baba34496daaa
Adam Giegel <adam.giegel@gmail.com>
2018-10-20 21:54:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.collection do |season, holiday|
+    holiday.collection do |holiday, type|
+      holiday if item.include?("BBQ")
+    end 
+  end.flatten.compact
 end
 
 
